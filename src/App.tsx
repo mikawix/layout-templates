98fcb42227b605394071fadb17ebee0e1a4b49c7
mikawix <mikan@wix.com>
2024-02-18 23:11:00 +0200

    default board changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,20 @@
-import { useState } from 'react';
-import classNames from 'classnames';
-import { ReactComponent as ReactLogo } from './assets/react.svg';
-import { ReactComponent as ViteLogo } from './assets/vite.svg';
-import { ReactComponent as TypescriptLogo } from './assets/typescript.svg';
-import { ReactComponent as ScssLogo } from './assets/scss.svg';
 import styles from './App.module.scss';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <div className={styles.App}>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <ViteLogo
-                        height="6em"
-                        width="6em"
-                        className={classNames(styles.logo)}
-                        title="Vite logo"
-                    />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <ReactLogo
-                        height="6em"
-                        width="6em"
-                        className={classNames(styles.logo, styles.react)}
-                        title="React logo"
-                    />
-                </a>
-                <a href="https://www.typescriptlang.org/" target="_blank">
-                    <TypescriptLogo
-                        height="6em"
-                        width="6em"
-                        className={classNames(styles.logo, styles.ts)}
-                        title="Typescript logo"
-                    />
-                </a>
-                <a href="https://sass-lang.com/" target="_blank">
-                    <ScssLogo
-                        height="6em"
-                        width="6em"
-                        className={classNames(styles.logo, styles.scss)}
-                        title="SCSS logo"
-                    />
-                </a>
-            </div>
             <div className={styles.card}>
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <h2>Welcome to your first board 🎉</h2>
                 <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
+                    Board is your rendering environment to edit your components. You can use boards
+                    to edit different variations of your components.
                 </p>
+                <p>Double click to edit your first component here.</p>
+                <div className={styles.slot}>Drag here elements from +Add elements panel</div>
             </div>
             <p className={styles['read-the-docs']}>
-                Click on the Vite and React logos to learn more
+                Click on preview and learn more here:
+                <a href="/">About Boards</a> |<a href="/">About Components</a>
             </p>
         </div>
     );
