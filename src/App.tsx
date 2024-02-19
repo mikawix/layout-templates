1eaa42a5eaa8fb70af95627beb389452dc7f0295
mikawix <mikan@wix.com>
2024-02-19 17:51:58 +0200

    inline style

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,43 @@ import styles from './App.module.scss';
 function App() {
     return (
         <div className={styles.App}>
-            <div className={styles.card}>
+            <div
+                style={{
+                    padding: '2em',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
                 <h2>Welcome to your first board 🎉</h2>
                 <p>
-                    Board is your rendering environment to edit your components. You can use boards
-                    to edit different variations of your components.
+                    Board is your rendering environment to edit your components. <br />
+                    You can use boards to edit different variations of your components. <br />
+                    Double click to edit your first component here.
                 </p>
-                <p>Double click to edit your first component here.</p>
-                <div className={styles.slot}>Drag here elements from +Add elements panel</div>
+                <div
+                    style={{
+                        width: '800px',
+                        height: '250px',
+                        borderRadius: '30px',
+                        border: '2px dashed #888',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        color: '#888888',
+                    }}
+                >
+                    Drag here elements from +Add elements panel
+                </div>
+                <p style={{ color: '#888888' }}>
+                    Click on preview and learn more here:{' '}
+                    <a href="https://help.codux.com/kb/en/article/what-are-boards">About Boards</a>{' '}
+                    |{' '}
+                    <a href="https://help.codux.com/kb/en/article/what-are-components">
+                        About Components
+                    </a>
+                </p>
             </div>
-            <p className={styles['read-the-docs']}>
-                Click on preview and learn more here:{' '}
-                <a href="https://help.codux.com/kb/en/article/what-are-boards">About Boards</a> |{' '}
-                <a href="https://help.codux.com/kb/en/article/what-are-components">
-                    About Components
-                </a>
-            </p>
         </div>
     );
 }
