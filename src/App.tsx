034d9f35c282c81aaa900c314ea5067a3940c229
mikawix <mikan@wix.com>
2024-03-05 18:45:06 +0200

    spacing update

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ import prev from './assets/preview-symbol.svg';
 function App() {
     return (
         <div className={styles.App}>
-            <h2>Welcome to your first Component 🎉</h2>
+            <h2>Welcome to your App Component 🎉</h2>
             Double click to edit App component. <br />
             Drag here elements from <b>+ Add elements panel</b>
+            <p style={{ fontSize: '12px' }}>
+                This project is using Vite + TypeScript. Visit vitejs.dev to learn more.
+            </p>
         </div>
     );
 }
